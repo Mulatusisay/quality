c95335e9912444094d676756f4c14867a475f08b
mulatu <mulatusisay1@gmail.com>
2016-04-05 19:26:57 +0300

    Initial commit

---
+++ grails-app/conf/BootStrap.groovy
@@ -0,0 +1,79 @@
+import quality.User
+import quality.Requestmap
+import quality.UserRole
+class BootStrap {
+
+    def init = { servletContext ->
+        println "Bootstrap is running"
+
+        // Request map Stored in Database
+        for (String url in [
+                '/', '/index', '/index.gsp', '/**/favicon.ico',
+                '/**/javascripts/**', '/**/stylesheets/**', '/**/images/**', '/assets/**',
+                '/login', '/login.*', '/login/*',
+                '/logout', '/logout.*', '/logout/*']) {
+            Requestmap.findOrSaveByUrlAndConfigAttribute(url, 'permitAll').save(flush: true, failOnError: true)
+        }
+        // Permit All
+        //  Requestmap.findOrSaveByUrlAndConfigAttribute('/dashboard/**', 'permitAll').save()
+        // Requestmap.findOrSaveByUrlAndConfigAttribute('/Schedule/**', 'permitAll').save(flush: true, failOnError: true)
+        // Requestmap.findOrSaveByUrlAndConfigAttribute('/trainingRecord/index.gsp', 'permitAll').save(flush: true, failOnError: true)
+        // Requestmap.findOrSaveByUrlAndConfigAttribute('/qcpc/index/*', 'permitAll').save(flush: true, failOnError: true)
+        //Requestmap.findOrSaveByUrlAndConfigAttribute('/qcpc/**', 'permitAll').save(flush: true, failOnError: true)
+        //Requestmap.findOrSaveByUrlAndConfigAttribute('/qcpc.*', 'permitAll').save(flush: true, failOnError: true)
+        //Requestmap.findOrSaveByUrlAndConfigAttribute('/qcpc/*', 'permitAll').save(flush: true, failOnError: true)
+
+        // Added for accessing .. by user name
+        // Requestmap.findOrSaveByUrlAndConfigAttribute('/blog/**', 'permitAll').save()
+
+        // Permitted to Logged In Users Only
+//        Requestmap.findOrSaveByUrlAndConfigAttribute('/staff/**', 'ROLE_ADMIN').save(flush: true, failOnError: true)
+//        Requestmap.findOrSaveByUrlAndConfigAttribute('/training/**', 'ROLE_ADMIN').save(flush: true, failOnError: true)
+//        Requestmap.findOrSaveByUrlAndConfigAttribute('/trainingRecord/**', 'isFullyAuthenticated()').save(flush: true, failOnError: true)
+//        Requestmap.findOrSaveByUrlAndConfigAttribute('/schedule/**', 'ROLE_ADMIN').save(flush: true, failOnError: true)
+//        Requestmap.findOrSaveByUrlAndConfigAttribute('/employeedetail/**', 'ROLE_ADMIN').save(flush: true, failOnError: true)
+//        Requestmap.findOrSaveByUrlAndConfigAttribute('/workdetail/create', 'ROLE_ADMIN').save(flush: true, failOnError: true)
+//        Requestmap.findOrSaveByUrlAndConfigAttribute('/workdetail/**', 'ROLE_ADMIN').save(flush: true, failOnError: true)
+//        Requestmap.findOrSaveByUrlAndConfigAttribute('/workdetail/index/*', 'ROLE_ADMIN').save(flush: true, failOnError: true)
+
+        //************************************************************************************************************
+
+        print("Test data generation in progress ...")
+
+        //************************************************************************************************************
+
+        println Environment.getCurrent()
+//
+        if (Environment.getCurrent() != Environment.PRODUCTION) {
+
+            User adminuser = User.findByUsername('admin')
+
+            /*  if(!user) {
+
+                  User.SaveByUsernameAndPassword(
+                          'admin', 'password'
+                  ).save(flush: true, failOnError: true)
+              }
+              if(null==user){
+                  User.SaveByUsernameAndPassword(
+                          'admin', 'password'
+                  ).save(flush: true, failOnError: true)
+              } */
+
+            //ccreate role
+            if (!adminuser) {
+                def adminRole = new Role(authority: 'ROLE_ADMIN')
+                assert adminRole.save(flush: true)
+                // Assign role to user
+                def adminUser = new User(username: 'admin', password: 'password')
+                assert adminUser.save(flush: true)
+                adminUser.validate()
+                println adminUser.errors
+                UserRole.create adminUser, adminRole, true
+            }
+            println "Bootstrap is done."
+        }//*/
+    }
+        def destroy = {
+        }
+    }
---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,44 @@
+dataSource {
+    pooled = true
+    driverClassName = "org.postgresql.Driver"
+    dialect = "org.hibernate.dialect.PostgreSQLDialect"
+}
+
+hibernate {
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+//    cache.region.factory_class = 'org.hibernate.cache.SingletonEhCacheRegionFactory' // Hibernate 3
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.SingletonEhCacheRegionFactory' // Hibernate 4
+    singleSession = true // configure OSIV singleSession mode
+    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
+}
+
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:postgresql://localhost:5432/quality"
+            username = "mulatu"
+            password = "1234"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:postgres://ayxbanpdqffnjz:71bImIgD9-XqQbcgW3C9EyiOrO@ec2-54-235-246-67.compute-1.amazonaws.com:5432/d5t2483h1bthgp?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
+            username = "ayxbanpdqffnjz"
+            password = "71bImIgD9-XqQbcgW3C9EyiOrO"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            // uri = new URI(System.env.DATABASE_URL?:"postgres://ejnyeihcbnjrbj:oap2WlaplWmYarMOtrMZyfU3jb@ec2-54-83-56-177.compute-1.amazonaws.com:5432/d7e2j4bet4voep")
+            uri = new URI(System.env.DATABASE_URL?:"postgres://ayxbanpdqffnjz:71bImIgD9-XqQbcgW3C9EyiOrO@ec2-54-235-246-67.compute-1.amazonaws.com:5432/d5t2483h1bthgp")
+            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
+        }
+    }
+}
